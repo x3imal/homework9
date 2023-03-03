8a21ac734e69eff3c05377443924f865c783b9ae
x3imal <aasurov@gmail.com>
2023-03-03 16:43:51 +0300

    1 task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Arrays;
+import java.util.SortedMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int j : arr) {
+            sum = sum + j;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        System.out.println(" ");
+
+    }
+
 }
